25a8c80b26b3a1fe47f3556be5d4194f57841982
arslan-7644539 <arslan7644539@gmail.com>
2024-12-27 11:33:23 +0500

    .

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddTodo from './Components/AddTodo'
+
+const App = () => {
+  return (
+    <>
+    <h1>My ToDo List</h1>
+    <AddTodo/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/AddTodo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AddTodo = () => {
+  return (
+    <div>
+      <form  >
+        <input type="text" placeholder='Enter a new Task' />
+        <button>Add Task</button>
+      </form>
+    </div>
+  )
+}
+
+export default AddTodo
